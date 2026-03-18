0575a626a279413f6c357d3ee3720b7a71c8bd12
kapiushoni <galechianarthur@gmail.com>
2026-03-18 22:03:25 +0200

    Add HTML

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
-import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
-app = FastAPI()
+app = FastAPI(title="Pretty Girl API")
 
-@app.get("/ask")
-def hello_index():
-    return {"You": "are a pretty girl)) kitty"}
+# Указываем папку с HTML файлом
+templates = Jinja2Templates(directory="templates")
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    # Теперь при заходе на сайт сразу будет открываться твой красивый интерфейс
+    return templates.TemplateResponse("index.html", {"request": request})
